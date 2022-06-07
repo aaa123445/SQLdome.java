322ee99cbf66ebced3f6e3ca848de8c94d14f4eb
aaa123445 <1178283274@QQ.COM>
2022-06-07 09:27:14 +0800

    加入游戏dome

--- src/test.java
+++ src/test.java
@@ -1,8 +1,11 @@
+import play.GameView;
+
 public class test {
     public static void main(String[] args) {
 
         SQLdome root2 = new SQLdome();
         root2.userregister("root2", "5201314");
         root2.login("sdas","sda");
+        new GameView().showView();
     }
 }
